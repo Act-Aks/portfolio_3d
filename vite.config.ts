879170b4b1154266d88f27949304e0406f3d4dc4
Act-Aks <akashsrivastava.git@gmail.com>
2023-11-15 18:18:51 +0530

    modified action and workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
       '@public': path.relative(__dirname, './public/'),
     },
   },
-  base: './',
+  server: {
+    port: 3000,
+    https: true,
+  },
+  preview: {
+    port: 3000,
+    https: true,
+  },
+  build: {
+    outDir: 'dist',
+    modulePreload: false,
+    minify: false,
+    cssCodeSplit: false,
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {},
+        minifyInternalExports: false,
+      },
+    },
+  },
 })
